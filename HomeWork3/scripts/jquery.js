f57faffca23f8aa2fa716117fcf93cf75421a9d2
Vinh Le <letanvinh0210@gmail.com>
2021-04-13 19:14:09 -0700

    Add files via upload

---
+++ HomeWork3/scripts/jquery.js
@@ -0,0 +1,19 @@
+// var myModal = document.getElementById("modalDemo");
+// // Set object for the button that will trigger the modal box
+// var myButton = document.getElementById("modalBtn");
+// // Set an element that will close the modal box
+// var exitBtn = document.getElementsByClassName("exit")[0];
+// // Allows display of modal box, when user clicks the button
+// myButton.onclick = function() {
+//     myModal.style.display = "block";
+// };
+// // Allows the user to close the modal box, when user will click on (x) button
+// exitBtn.onclick = function() {
+//     myModal.style.display = "none";
+// };
+// // Allows the user to close the modal box, even when the user clicks anywhere outside of the modal box
+// window.onclick = function(event) {
+//     if (event.target == myModal) {
+//         myModal.style.display = "none";
+//     }   
+// };
